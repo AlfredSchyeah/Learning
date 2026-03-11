b205fa13ff169bb7acdfabd6e3c3adf882b3d269
Alfred Shayahmetov <ialfred707@gmail.com>
2026-03-11 16:20:51 +0300

    Add variable declaration example with console logging

---
+++ javascript/002-variable-declaration/ex-self.js
@@ -0,0 +1,11 @@
+let firstname = "Leonardo";
+let lastname = "da Vinci";
+
+console.log(firstname);
+console.log(lastname);
+
+firstname = "Michelangelo";
+lastname = "Buonarroti";
+
+console.log(firstname);
+console.log(lastname);
