80cd6a9532a9e5718799e6a6772d45944445a7ed
Alfred Shayahmetov <ialfred707@gmail.com>
2026-03-12 20:00:37 +0300

    6 урок преобразования js

---
+++ javascript/006-type-conversion/self-ex-01.js
@@ -0,0 +1,6 @@
+console.log(Number.isNaN(NaN));
+console.log(Number.isNaN(10 / 0));
+console.log(Number.isNaN("NaN"));
+console.log(Number.isNaN(undefined));
+console.log(isNaN(0 / 0));
+console.log(10 / 0);
