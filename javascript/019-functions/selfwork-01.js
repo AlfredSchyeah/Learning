5c25e71a7224af8179d231c39ad04ec9872371cb
Alfred Shayahmetov <ialfred707@gmail.com>
2026-03-16 14:35:28 +0300

    19 selfwork

---
+++ javascript/019-functions/selfwork-01.js
@@ -0,0 +1,24 @@
+function greeting() {
+    console.log('Hello!')
+}
+
+greeting();
+
+function howareu(name) {
+    console.log(`Hello how are you ${name} ?`);
+}
+
+howareu('Alex');
+
+function answer() {
+    return 'Feel good!'
+}
+
+console.log(answer());
+
+function simplemath(x, y) {
+    let sum = x + y;
+    return sum;
+}
+
+console.log(simplemath(3, 7))
