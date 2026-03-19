96db2771db547471f72a4a8c9d9c3f56d286736f
Alfred Shayahmetov <ialfred707@gmail.com>
2026-03-20 01:33:36 +0300

    import export functions 28

---
+++ javascript/028-multiple-files-project/selfwork/index.js
@@ -0,0 +1,13 @@
+import * as myMath from "./my-math.js";
+
+console.log(myMath.multiply10(10));
+console.log(myMath.minus(7, 6));
+
+import { multiply, divide } from "./allexp.js";
+
+console.log(multiply(5, 4));
+console.log(divide(20, 5));
+
+import substract from "./def-substract.js";
+
+console.log(substract(10, 3));
