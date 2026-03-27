3539b28bd9c0e4f3493f1faa1b818c5dce96faff
Alfred Shayahmetov <Alfredmerc@yandex.ru>
2026-03-27 17:09:23 +0300

    Викторина

---
+++ DOTNET-CODE/tip-calculator/questionnaire/Program.cs
@@ -0,0 +1,39 @@
+﻿int sum = default;
+
+Console.Write("Столица Испании? [Барселона], [Мадрид], [Париж] ");
+string Spain = Console.ReadLine();
+if (Spain.ToLowerInvariant() == "мадрид")
+{
+    Console.Write("Правильно!");
+    sum += 1;
+}
+else
+{
+    Console.WriteLine("😭");
+}
+
+Console.Write("Столица Италии? [Рим], [Флоренция], [Милан] ");
+string Italy = Console.ReadLine();
+if (Italy.ToLowerInvariant() == "рим")
+{
+    Console.Write("Правильно!");
+    sum += 1;
+}
+else
+{
+    Console.WriteLine("😭");
+}
+
+Console.Write("Столица Китая? [Пекин], [Шанхай], [Гонконг] ");
+string China = Console.ReadLine();
+if (China.ToLowerInvariant() == "пекин")
+{
+    Console.Write("Правильно!");
+    sum += 1;
+}
+else
+{
+    Console.WriteLine("😭");
+}
+
+Console.WriteLine($"Ваш результат: {sum}");
