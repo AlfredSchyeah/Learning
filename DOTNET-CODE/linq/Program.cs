a64099216ad03ae92cf092b992a8795810016e6a
Alfred Shayahmetov <Alfredmerc@yandex.ru>
2026-04-03 15:55:55 +0300

    Linq

---
+++ DOTNET-CODE/linq/Program.cs
@@ -0,0 +1,9 @@
+﻿List<int> ints = Enumerable.Range(0, 10)
+    .Select(item => Random.Shared.Next(10))
+    .Select(item => item * 7)
+    .Where(item => item % 2 == 0)
+    .OrderBy(item => item)
+    .ToList();
+
+Console.WriteLine(ints.Count);
+Console.WriteLine(String.Join(", ", ints));
