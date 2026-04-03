6439c3b859cd17d7dd97db1c893398e8b32a5f74
Alfred Shayahmetov <Alfredmerc@yandex.ru>
2026-04-03 15:39:35 +0300

    Делегаты

---
+++ DOTNET-CODE/delegate/Program.cs
@@ -0,0 +1,38 @@
+﻿// bool IsEven(int x)
+// {
+//     return x % 2 == 0;
+// }
+
+int[] CheckArray(int[] numbers, Func<int, bool> logic)
+{
+    int count = numbers.Length;
+    int countEven = 0;
+
+    for (int i = 0; i < count; i++)
+    {
+        if (logic(numbers[i]))
+        {
+            countEven++;
+        }
+    }
+
+    int[] evenNumbers = new int[countEven];
+    int pos = 0;
+    for (int i = 0; i < count; i++)
+    {
+        if (logic(numbers[i]))
+        {
+            evenNumbers[pos] = numbers[i];
+            pos++;
+        }
+    }
+    return evenNumbers;
+}
+
+int[] ints = CheckArray(new int[] { 1, 2, 3, 4, 5 }, x => x % 2 == 0);
+
+
+
+Console.WriteLine(String.Join(", ", ints));
+
+// delegate bool MyMethodDelegate(int value);
