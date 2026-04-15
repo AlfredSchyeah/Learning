e4f035f892bd549af61ec65cde5484d58f767375
Alfred Shayahmetov <Alfredmerc@yandex.ru>
2026-04-15 13:23:18 +0300

    Async

---
+++ async-await/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Запрашиваем данные...");
+string result = await GetDataAsync();
+Console.WriteLine(result);
+
+static async Task<string> GetDataAsync()
+{
+    await Task.Delay(20000);
+    return "Данные получены!";
+}
